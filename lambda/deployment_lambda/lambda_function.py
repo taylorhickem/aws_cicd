13197baaeae33eddffda091bb832a1d04fa3f9d8
taylorhickem <taylor.hickem@gmail.com>
2023-12-30 18:50:59 +0800

    walk subdirectories

--- lambda/deployment_lambda/lambda_function.py
+++ lambda/deployment_lambda/lambda_function.py
@@ -87,36 +87,51 @@ def function_update(function_name):
 
 
 def source_code_zip_from_s3(function_name):
+    source_code_zip = None
+    files = []
     local_dir = f'{TMP_DIR}/{function_name}'
     if not os.path.exists(local_dir):
         os.mkdir(local_dir)
     function_dir = f'{S3_DIR}/{function_name}'
 
     client_load('s3')
-    objects = clients['s3'].list_objects_v2(
+    response = clients['s3'].list_objects_v2(
         Bucket=S3_BUCKET,
         Prefix=function_dir
     )
-    print(objects['Contents'])
-    for obj in objects['Contents']:
-        f = obj['Key']
-        if f not in [f'{S3_DIR}/', f'{function_dir}/']:
-            file_name = f.split('/')[-1]
-            local_path = f'{local_dir}/{file_name}'
+    contents = response['Contents']
+    print(f'S3 source code contents for lambda function {function_name}: \n {contents}')
+    if len(contents) > 0:
+        for obj in contents:
+            key = obj['Key']
+            if key not in [f'{S3_DIR}/', f'{function_dir}/']:
+                files.append(key)
+
+    if len(files) > 0:
+        for f in files:
+            local_path = f.replace(function_dir, local_dir, 1)
+            if not os.path.exists(os.path.dirname(local_path)):
+                os.makedirs(os.path.dirname(local_path))
             clients['s3'].download_file(S3_BUCKET, f, local_path)
     client_unload('s3')
 
+    print('source code files:')
+    for subdir, dirs, files in os.walk(local_dir):
+        for file in files:
+            print(os.path.join(subdir, file))
+
     #zip the files
-    zip_bytes = io.BytesIO()
-    with zipfile.ZipFile(zip_bytes, 'w') as zipf:
-        for root, _, files in os.walk(local_dir):
-            for f in files:
-                file_path = os.path.join(root, f)
-                zipf.write(
-                    file_path,
-                    os.path.relpath(file_path, local_dir)
-                )
-    source_code_zip = zip_bytes.getvalue()
+    if len(files) > 0:
+        zip_bytes = io.BytesIO()
+        with zipfile.ZipFile(zip_bytes, 'w') as zipf:
+            for root, _, files in os.walk(local_dir):
+                for f in files:
+                    file_path = os.path.join(root, f)
+                    zipf.write(
+                        file_path,
+                        os.path.relpath(file_path, local_dir)
+                    )
+        source_code_zip = zip_bytes.getvalue()
 
     shutil.rmtree(TMP_DIR, ignore_errors=True)
     return source_code_zip
